07e6e9af911ce95c3356a1dd3f32796664db0b05
haoniu08 <niu.hao@northeastern.edu>
2025-10-30 22:39:58 -0700

    Push omitted files

--- internal/models/cart.go
+++ internal/models/cart.go
@@ -3,17 +3,15 @@ package models
 import "time"
 
 type CartItem struct {
-  ProductID int64 `json:"product_id"`
-  Quantity  int   `json:"quantity"`
+	ProductID int64 `json:"product_id"`
+	Quantity  int   `json:"quantity"`
 }
 
 type Cart struct {
-  ShoppingCartID int64      `json:"shopping_cart_id"`
-  CustomerID     int64      `json:"customer_id"`
-  Status         string     `json:"status"`
-  Items          []CartItem `json:"items"`
-  CreatedAt      time.Time  `json:"created_at"`
-  UpdatedAt      time.Time  `json:"updated_at"`
+	ShoppingCartID int64      `json:"shopping_cart_id"`
+	CustomerID     int64      `json:"customer_id"`
+	Status         string     `json:"status"`
+	Items          []CartItem `json:"items"`
+	CreatedAt      time.Time  `json:"created_at"`
+	UpdatedAt      time.Time  `json:"updated_at"`
 }
-
-
